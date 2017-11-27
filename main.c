b204e26728f0a895cc987979feb49329c0c833f7
msyounge <32930636+msyounge@users.noreply.github.com>
2017-11-27 18:02:29 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,16 +6,16 @@ int main()
     
     while (scanf_s("%d", &num, sizeof(int)) != EOF)
     {
-        Makenode(num);
+        Make_node(num);
     }
     
     printf("%d\n", The_number_of_node());
     
-    Printmidle();
-    Printall();
-    Printrev();
-    Deleteoddnode();
-    Printall();
+    Print_middle();
+    Print_all();
+    Print_rev();
+    Delete_odd_node();
+    Print_all();
     
     return 0;
 }
