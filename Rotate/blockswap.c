66fe68fedced623fe5d3b296c3487ebbf86c388b
YangSuyeong <didtndud0186@hanmail.net>
2017-12-05 12:51:32 +0900

    sususususu

--- Rotate/blockswap.c
+++ Rotate/blockswap.c
@@ -1,34 +1,38 @@
-#include "Rotate.h"
 
-void blockswap(char* str, int n, int d)
+void Swap(char arr[], int a, int b, int d)
 {
-    int i,j;
-    int swap;
-    char *temp;
-    if(d>0)
-    {
-        swap = n - d;
-        temp = (char*)malloc(sizeof(char)*d);
-        for(i=0;i<d;i++)
-            temp[i] = str[i];
-        for(i=d,j=0;i<n;i++,j++)
-            str[j]=str[i];
-        for(i=0;i<d;i++)
-            str[swap++]=temp[i];
-        free(temp);
-    }
-    else
-    {
-        int Dcal=-d;
-        swap = n - Dcal;
-        temp = (char*)malloc(sizeof(char)*Dcal);
-        for(i=swap,j=0;i<n;i++,j++)
-            temp[j]=str[i];
-        for(i=n-1,j=swap-1;j>=0;i--,j--)
-            str[j]=str[i];
-        for(i=0;i<Dcal;i++)
-            str[i]=temp[i];
-        free(temp);
-    }
+	char temp;
+	for (int i = 0; i < d; ++i)
+	{
+		temp = arr[a + i];
+		arr[a + i] = arr[b + i];
+		arr[b + i] = temp;
+	}
+}
+
+char* Blockswap(char arr[], int d, int n)
+{
+	if (d == 0 || d == n)
+		return arr;
+
+
+	if (n - d == d)
+	{
+		Swap(arr, 0, n - d, d);
+		return arr;
+	}
+
+
+	if (d < n - d)
+	{
+		Swap(arr, 0, n - d, d);
+		Blockswap(arr, d, n - d);
+	}
+	else
+	{
+		Swap(arr, 0, d, n - d);
+		Blockswap(arr + n - d, 2 * d - n, d);
+	}
+	return arr;
 }
 
